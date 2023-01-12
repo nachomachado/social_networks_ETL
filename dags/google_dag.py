427419065928e324cb3348e68a43454e58743141
Lucio Suppo <lucio.suppo@gmail.com>
2023-01-12 10:05:04 -0300

    creacion del dag y dockerfile

---
+++ dags/google_dag.py
@@ -0,0 +1,25 @@
+from datetime import timedelta
+from airflow import DAG
+
+from airflow.operators.python import PythonOperator
+from airflow.utils.dates import days_ago
+from tools.main import estract
+from common_args import default_args
+
+with DAG(
+    'google imports',
+    default_args=default_args,
+    description='DAG para importar datos de google',
+    schedule_interval=timedelta(days=1),
+    start_date=days_ago(8),
+    end_date=days_ago(6),
+    tags=['aca va el tag'],
+) as dag:
+
+    estraction = PythonOperator(
+        task_id='run_estraction_process',
+        python_callable=estract,
+        dag=dag,
+    )
+
+estraction
